7dc54b1ca569470170d408345c812c1a049c2bf6
WANWISAAA <chumpornw6@gmail.com>
2025-08-19 12:04:43 +0700

    first commit

---
+++ app/components/app/header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="font-[sans-serif] bg-gray-200">
+        <div class="text-gray-600-bg[#FA8072]">
+        </div class="container px-6 py-[0.8rem] mx-auto md:flex md:justify-">
+        <nav>
+            <h1>Header</h1>
+        </nav>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center text-blue-500 font-semibold">
+    <div class="flex justify-center items-center">
+      <img src="https://nuxt.com/assets/design-kit/icon-green.svg" alt="logo">
+    </div>
+    <p class="my-4">Home</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
